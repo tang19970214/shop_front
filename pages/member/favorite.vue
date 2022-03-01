5c1a0bbb6f72db2b0572a744a3c0010ee333fab3
George <tang19970214@gmail.com>
2022-03-01 10:33:04 +0800

    upload

--- pages/member/favorite.vue
+++ pages/member/favorite.vue
@@ -1,3 +1,56 @@
 <template>
-  <section class="w-full text-center">favorite</section>
+  <section class="w-full text-center">
+    <div class="w-full grid grid-cols-3 gap-6">
+      <div class="w-full flex flex-col gap-1 text-left" v-for="item in filterList(list)" :key="item.id">
+        <img :src="item.imgUrl" :alt="item.title" />
+
+        <label>{{ item.title }}</label>
+
+        <div class="w-full flex items-center justify-between">
+          <div class="flex items-center gap-1">
+            <p class="text-red-500">${{ item.sale > 0 ? item.sale : item.price }}</p>
+            <p class="text-sm text-neutral-400 line-through" v-if="item.sale > 0">${{ item.price }}</p>
+          </div>
+
+          <fa class="cursor-pointer" :class="{ 'text-red-600': item.favorite }" :icon="[item.favorite ? 'fas' : 'far', 'heart']" @click="joinFavorite(item)" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        { id: 1, imgUrl: require("~/static/images/product_example.png"), title: "春季的初戀微甜｜金牌紅烏龍禮盒", price: 6520, sale: 6000, favorite: true, inStock: 1 },
+        { id: 2, imgUrl: require("~/static/images/product_example.png"), title: "春季的初戀微甜｜金牌紅烏龍禮盒", price: 6520, sale: 6000, favorite: true, inStock: 1 },
+        { id: 3, imgUrl: require("~/static/images/product_example.png"), title: "春季的初戀微甜｜金牌紅烏龍禮盒", price: 6000, sale: 0, favorite: true, inStock: 0 },
+        { id: 4, imgUrl: require("~/static/images/product_example.png"), title: "春季的初戀微甜｜金牌紅烏龍禮盒", price: 6000, sale: 0, favorite: true, inStock: 1 },
+        { id: 5, imgUrl: require("~/static/images/product_example.png"), title: "春季的初戀微甜｜金牌紅烏龍禮盒", price: 6000, sale: 0, favorite: true, inStock: 1 },
+      ],
+    };
+  },
+  computed: {
+    // TODO: 介接api後移除此篩選
+    filterList() {
+      return (list) => {
+        return list.filter((i) => i.favorite);
+      };
+    },
+  },
+  methods: {
+    joinFavorite(item) {
+      item.favorite = !item.favorite;
+
+      this.$swal.fire({
+        icon: "success",
+        title: "已取消收藏",
+        timer: 1500,
+        showConfirmButton: false,
+      });
+    },
+  },
+};
+</script>
