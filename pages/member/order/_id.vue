306c38d956bf771a6c82cb312b3304c227c1d671
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2022-03-07 17:12:05 +0800

    fix

--- pages/member/order/_id.vue
+++ pages/member/order/_id.vue
@@ -194,17 +194,24 @@
     </div>
 
     <!-- 取消訂單彈出視窗 -->
-    <Modal :openModal="isOpen" @closeModal="closeModal">
+    <Modal :openModal="isOpen" :width="'w-11/12 lg:w-1/2'" @closeModal="closeModal">
       <div class="flex flex-col items-center">
-        <h3 class="text-2xl font-bold mb-8">訂單已取消</h3>
+        <div class="flex">
+          <h3 class="text-2xl font-bold mb-8">訂單已取消</h3>
+          <div class="ml-4 flex justify-center w-8 h-8 items-center rounded-full border-2 border-[#16A34A]">
+            <fa class="text-sm text-[#16A34A] font-bold" icon="fa-sold fa-check" />
+          </div>
+        </div>
         <div class="flex items-center">
-          <span class="text-lg">取消原因：</span>
-          <select v-model="cancelType" name="" class="text-slate-300 py-2.5 px-4 w-60 rounded-md border border-[#a3a3a3]" :class="{ 'text-slate-900': cancelType !== '' }">
-            <option value="" disabled selected>請選擇</option>
-            <option :value="item" v-for="item in cancelOrderSelect" :key="item">
-              {{ item }}
-            </option>
-          </select>
+          <span class="text-sm lg:text-lg">取消原因：</span>
+          <div class="w-60 relative after:absolute after:pointer-events-none after:w-6 after:h-full after:content-[''] after:top-4 after:right-3 after:bg-[url(~/static/images/icon/arrow-bottom.svg)] after:bg-no-repeat">
+            <select v-model="cancelType" name="" class="appearance-none py-2.5 px-4 w-full rounded-md border border-[#a3a3a3]" :class="{ 'text-slate-400': cancelType === '','text-slate-900': cancelType !== '' }">
+              <option value="" disabled selected>請選擇</option>
+              <option :value="item" v-for="item in cancelOrderSelect" :key="item">
+                {{ item }}
+              </option>
+            </select>
+          </div>
         </div>
         <button @click="cancelOrder()" data-mdb-ripple="true" data-mdb-ripple-color="light" class="duration-150 mt-16 mb-14 w-60 py-3.5 px-2 rounded-lg shadow-md text-white text-lg tracking-widest bg-gradient-to-r from-[#FA5936] to-[#FF6D3F] hover:shadow-inner disabled:cursor-not-allowed disabled:opacity-80">確認</button>
       </div>
