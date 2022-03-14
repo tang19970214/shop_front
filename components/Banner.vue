63897128661c18e5e3ed847af6767ecf6ca1f52b
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2022-03-14 17:54:05 +0800

    fix

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,29 +1,20 @@
 <template>
-  <div>
-    <hooper class="min-h-screen" :wheelControl="false" :autoPlay="true" :playSpeed="5000" :transition="1000" :infiniteScroll="true">
-      <slide v-for="carousel in carouselArr" :key="carousel.text">
-        <div class="w-screen h-[50vg] md:h-[80vh] lg:h-screen relative">
-          <a class="w-full h-full select-none">
-            <img class="w-full h-full object-cover pointer-events-none" :src="carousel.img" :alt="carousel.text">
-            <h2 class="w-2/3 text-center text-4xl md:text-5xl text-white mix-blend-difference absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-              {{ carousel.text }}
-            </h2>
-          </a>
-        </div>
-      </slide>
-    </hooper>
-  </div>
+  <section>
+    <VueSlickCarousel :arrows="false">
+      <div v-for="carousel in carouselArr" :key="carousel.text" class="w-screen h-[50vh] md:h-[80vh] lg:h-screen relative">
+        <a href="#" class="pointer-events-none">
+          <img class="w-full pointer-events-none" :src="carousel.img" :alt="carousel.text">
+        </a>
+        <h2 class="w-2/3 text-center text-4xl md:text-5xl text-white mix-blend-difference absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          {{ carousel.text }}
+        </h2>
+      </div>
+    </VueSlickCarousel>
+  </section>
 </template>
 
 <script>
-import { Hooper, Slide } from "hooper";
-import "hooper/dist/hooper.css";
-
 export default {
-  components: {
-    Hooper,
-    Slide,
-  },
   props: {
     carouselArr: {
       type: Array,
