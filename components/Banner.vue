4dea1e1cf0dcc449e009549c75f7fe3680ebf1d9
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2022-03-14 18:04:10 +0800

    fix

--- components/Banner.vue
+++ components/Banner.vue
@@ -3,9 +3,9 @@
     <VueSlickCarousel :arrows="false">
       <div v-for="carousel in carouselArr" :key="carousel.text" class="w-screen h-[50vh] md:h-[80vh] lg:h-screen relative">
         <a href="#" class="pointer-events-none">
-          <img class="w-full pointer-events-none" :src="carousel.img" :alt="carousel.text">
+          <img class="w-full pointer-events-none h-full object-cover" :src="carousel.img" :alt="carousel.text">
         </a>
-        <h2 class="w-2/3 text-center text-4xl md:text-5xl text-white mix-blend-difference absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h2 class="w-full md:w-2/3 text-center text-4xl md:text-5xl text-white mix-blend-difference absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           {{ carousel.text }}
         </h2>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
 
       <button type="button" class="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" @click="goLogin()">Login</button>
 
-      <button @click="getMemberByToken()" class="shadow-md duration-150 text-pink-400 border-2 border-pink-400 rounded-2xl p-3 mt-5 hover:bg-pink-400 hover:text-white">GET_MEMBER_BY_TOKEN</button>
+      <!-- <button @click="getMemberByToken()" class="shadow-md duration-150 text-pink-400 border-2 border-pink-400 rounded-2xl p-3 mt-5 hover:bg-pink-400 hover:text-white">GET_MEMBER_BY_TOKEN</button> -->
     </div>
 
     <div class="w-full" v-for="item in blockList" :key="item.id">
